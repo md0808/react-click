6b0feef2a3f4ed27146ae92dadc54267393bd3c4
Monica <monica.dixon8@gmail.com>
2020-01-31 10:01:53 -0600

    removed comments

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,39 +4,24 @@ import Scoreboard from "../Scoreboard";
 import plants from "../plantList.json";
 import "./style.css";
 
-//This is where the majority of the game logic will go.
-//Within the gameboard component I'll need to map the randomized array of plants into cards
-
-//
 const instructions = [
   "Click on a plant to begin the game",
-  //changes to [1] once game has started. [2] when the second click occurs
   "Keep clicking!",
   "Start over!"
 ];
 
 class Container extends Component {
-  //this sets the initial state, when everything is loaded. On click events with trigger changes in state
   state = {
-    //pass on click to scoreboard
-
-    //Direction state will trigger
     direction: instructions[0],
-    //This will be changed by the arrayShuffle once I get the images showing.
     plantsArray: plants,
-    //This will be given to the Gameboard component, it will show the user the name of the plant they clicked on most recently.
     justClicked: "",
-    //this will hold an array of the id's of the cards that have been clicked
     clicked: [],
-    //This will increase each time until the user double clicks, then reset to 0
     currentScore: 0,
-    // if current score >= high score, replaces high score
     highScore: 0,
     outcome: 1
   };
 
   handleClick = (id, name) => {
-    // this is triggered in the card component. How doe s
     const clickedArray = [...this.state.clicked];
     let score = this.state.currentScore;
     let currentHighScore = this.state.highScore;
@@ -67,21 +52,9 @@ class Container extends Component {
       });
       console.log("doesn't include");
     }
-
-    // on click, needs to check  this.state.clicked to see if the id is already in the array of clicked objects.
-    // if the object has already been clicked:
-    //game over - current score is reset to zero
-    //set directions to instructions[2]
-    // else increase current score +1
-    //if currentscore >= highscore, set high score
-    //set just clicked which sends the name of the plant they just clicked to the gameboard component.
-    // then triggers arrayShuffle(this.state.plantsArray)
-    // updates the plantsArray state to arrayshuffle.
   };
 
   arrayShuffle() {
-    // Takes in the array of plants from the json
-    //Durstenfeld Shuffle
     let tempArray = [...plants];
     for (let i = tempArray.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -92,7 +65,6 @@ class Container extends Component {
     return tempArray;
   }
 
-  // this is what rerenders when react senses a change.
   render() {
     console.log(this.state.currentScore);
     return (
