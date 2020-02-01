d3a9ff48ca9a39c882f722e52584c0b3cef444e6
Monica <monica.dixon8@gmail.com>
2020-02-01 10:16:06 -0600

    Updates

---
+++ precache-manifest.a15e529ecfd098c66952083934c9b652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7691d1b60665fe826b719fbc791f5e",
+    "url": "/react-click/index.html"
+  },
+  {
+    "revision": "03b3390afdb3e3759e0e",
+    "url": "/react-click/static/css/main.e8427268.chunk.css"
+  },
+  {
+    "revision": "68d4f230467ecfecc3cf",
+    "url": "/react-click/static/js/2.c0a87905.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-click/static/js/2.c0a87905.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03b3390afdb3e3759e0e",
+    "url": "/react-click/static/js/main.b31eb931.chunk.js"
+  },
+  {
+    "revision": "36c3510ca110b5623a53",
+    "url": "/react-click/static/js/runtime-main.6119353d.js"
+  }
+]);
